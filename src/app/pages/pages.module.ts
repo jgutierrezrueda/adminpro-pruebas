1118dc73ce71d0374f818e061086dc7b2a67fae3
Jaime Gutiérrez Rueda <jgutierrez@talentosoftware.com>
2020-04-11 12:29:22 +0200

    add modules

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,36 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+// routes
+import {PAGES_ROUTES} from './pages.routes';
+
+// modules
+import {SharedModule} from '../shared/shared.module';
+
+// components
+import {PagesComponent} from './pages.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {ProgressComponent} from './progress/progress.component';
+import {Graficas1Component} from './graficas1/graficas1.component';
+
+
+@NgModule({
+  declarations: [
+    PagesComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    PAGES_ROUTES
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+  ]
+})
+export class PagesModule {
+}
